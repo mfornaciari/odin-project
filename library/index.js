f18da4e1953b949b08a6e8a1e28b8f11e03fd823
Marco Fornaciari <marco.fornaciari@rebase.com.br>
2022-10-10 09:21:50 -0300

    Create book adding feature

--- library/index.js
+++ library/index.js
@@ -1,5 +1,3 @@
-let library = [];
-
 class Book {
   constructor({ title = '', author = '', pages = 0, read = false }) {
     this.title = title;
@@ -14,7 +12,22 @@ class Book {
   }
 }
 
-function addBookToLibrary({ title = '', author = '', pages = 0, read = false }) {
-  const bookInfo = { title, author, pages, read };
+function addBookToLibrary(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const dataObject = Object.fromEntries(formData);
+  const bookInfo = convertToBookInfo(dataObject);
   library.push(new Book(bookInfo));
 }
+
+function convertToBookInfo(dataObject) {
+  return {
+    ...dataObject,
+    pages: parseInt(dataObject.pages),
+    read: dataObject.read ? true : false,
+  };
+}
+
+const library = [];
+const form = document.getElementById('form');
+form.addEventListener('submit', addBookToLibrary);
